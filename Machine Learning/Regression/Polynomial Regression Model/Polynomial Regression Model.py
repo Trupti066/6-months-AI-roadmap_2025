177de10383a895bcb7f003b2feba0d3534f58fa3
Trupti066 <truptiarati066@gmail.com>
2025-08-30 08:17:55 +0530

    Add files via upload

---
+++ Machine Learning/Regression/Polynomial Regression Model/Polynomial Regression Model.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv(r"C:\Ds & AI ( my work)\Machine Learning\Polynomial Regression Model\emp_sal.csv")
+
+x = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:,2].values
+
+#Linear model -- linear algor (degree -1)
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(x,y)
+
+plt.scatter(x, y, color = 'red')
+plt.plot_date(x, lin_reg.predict(x), color = 'blue')
+plt.title('Linear regression model (Linear Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+lin_model_pred = lin_reg.predict([[6]])
+lin_model_pred
+
+# Modeling Polynomial Regression Model
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree= 5)
+x_poly = poly_reg.fit_transform(x)
+
+poly_reg.fit(x_poly, y)
+
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(x_poly, y)
+
+plt.scatter(x, y, color = 'red')
+plt.plot_date(x, lin_reg_2.predict(poly_reg.fit_transform(x)), color = 'blue')
+plt.title('polymodel (Polynomial Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+poly_model_pred = lin_reg_2.predict(poly_reg.fit_transform([[6]]))
+poly_model_pred
+
+
+
+
+
+
+
+
+
+
+
+
+
