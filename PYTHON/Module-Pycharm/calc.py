3f52f99beb1e002b0aafbd75c92b8f0a0a1dbcd7
Trupti066 <truptiarati066@gmail.com>
2025-09-10 21:43:34 +0530

    Add files via upload

---
+++ PYTHON/Module-Pycharm/calc.py
@@ -0,0 +1,20 @@
+from test import *    # now we creating our own module test
+
+a = 10
+b = 5
+
+di = div(a,b)
+ad = add(a,b)
+mu = mul(a,b)
+sub = sub(a,b)
+mul = mul(a,b)
+div = div(a,b)
+mod = mod(a,b)
+
+print(di)
+print(ad)
+print(mu)
+print(sub)
+print(mul)
+print(div)
+print(mod)
